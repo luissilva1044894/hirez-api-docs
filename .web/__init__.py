6b522971d1b68e1ada1ea72cda482a456f22d185
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-11 17:15:36 -0300

    Update __init__.py

--- .web/__init__.py
+++ .web/__init__.py
@@ -1,15 +1,23 @@
 
 import os
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for, send_from_directory
 
 def create_app(*args, **kw):
-	app = Flask(kw.pop('name', __name__), static_folder=kw.pop('static_folder', './../_assets'), static_url_path=kw.pop('static_folder', ''), **kw)
+  app = Flask(kw.pop('name', __name__), static_folder=kw.pop('static_folder', os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '_assets')), **kw)#, static_url_path=kw.pop('static_folder', '')
 
-	@app.route('/', methods=['GET'])
-	def root():
-		return redirect('https://github.com/luissilva1044894/hirez-api-docs/blob/master/README.md')
+  @app.route('/', methods=['GET'])
+  def root():
+    return redirect('https://github.com/luissilva1044894/hirez-api-docs/blob/master/README.md')
 
-	return app
+  @app.route('/<game>/avatar/<avatar_id>/', strict_slashes=False)
+  def legacy_images(avatar_id, game='paladins'):
+    path = os.path.join(app.static_folder, game, 'avatar')
+    for _ in os.listdir(path):
+      if _.split('.', 1)[0] == avatar_id:
+        return send_from_directory(path, _)
+    return send_from_directory(path, '0.png')
+
+  return app
 
 def main():
   create_app().run()
