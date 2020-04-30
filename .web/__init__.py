c7e0813406446e4a6e96134623ced24cadcec4c3
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-04-30 11:46:06 -0300

    Update __init__.py

--- .web/__init__.py
+++ .web/__init__.py
@@ -90,7 +90,7 @@ def create_app(*args, **kw):
   @app.route('/paladins/avatar/', defaults={'avatar_id': 0}, methods=['GET', 'POST'])
   @app.route('/paladins/avatar/<avatar_id>', methods=['GET', 'POST'])
   def legacy_images(avatar_id, game='paladins', folder='avatar'):
-    path, _avatar_id = os.path.join(app.static_folder, game, folder), AvatarId(get_avatar_id(avatar_id, request))
+    path, _avatar_id = os.path.join(app.static_folder, game, folder), AvatarId(avatar_id)#AvatarId(get_avatar_id(avatar_id, request))
     print(_avatar_id)
     for _ in os.listdir(path):
       if AvatarId(_.split('.', 1)[0]) == _avatar_id:
