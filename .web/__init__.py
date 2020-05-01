dac70540be58267222ec880618646b282c48e289
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-05-01 12:43:00 -0300

    Update __init__.py

--- .web/__init__.py
+++ .web/__init__.py
@@ -140,7 +140,7 @@ def create_app(*args, **kw):
            'format': _.split('.', 1)[-1],
            'id': int(_avatar_id),#_.split('.', 1)[0],
            'name': str(_avatar_id),
-           'path': f'{game}/{folder}/{_}',
+           'path': f'{game}/{folder}/{int(_avatar_id)}',
            'remote_path': url_for('static', filename=f'{game}/{folder}/{_}'),
            'updated_at': get_file_date(f_path),
            'url': url_for('static', filename=f'{game}/{folder}/{_}', _external=True)
