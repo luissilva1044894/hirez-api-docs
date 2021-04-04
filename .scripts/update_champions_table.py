bd9570effd3480a7d79ab668de160e55afa6e267
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2021-04-04 20:04:06 -0300

    Update Champions Table script

--- .scripts/update_champions_table.py
+++ .scripts/update_champions_table.py
@@ -1,27 +1,33 @@
 template = """<details markdown="1">
 <summary>Champions</summary>
 
-There are currently {LEN} playable champions (Updated in {DATE}):
+There are currently {LEN} playable champions in the game (Updated in {DATE}):
 <table>
   <tr>
     <th>ID</th>
     <th>Name</th>
     <th>Role</th>
     <th>Title</th>
-    <th>Image</th>
+    <th>Portrait</th>
+    <th>Header</th>
+    <th>Carousel</th>
   </tr>
   {CHAMPS}
   </table>
 </details>"""
-import requests
+import httpx
 import sys
 from datetime import datetime
 
 def fix_name(o):
   return str(o).replace(' ', '-').replace("'", '').lower()
 def create_value(_):
-  return '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td><img src="https://web2.hirez.com/paladins/champion-icons/{}.jpg" height="64" width="64"/></td></tr>'.format(_.get('id'), _.get('feName'), _.get('role').split(' ', 1)[1].replace('er', ''), _.get('title'), fix_name(_.get('feName')))
-_json_ = [f'{create_value(_)}' for _ in requests.get('https://cms.paladins.com/wp-json/api/champion-hub/1').json() or {} if _]
+  _name = fix_name(_.get('feName'))
+  return '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td><img src="https://webcdn.hirezstudios.com/paladins/champion-icons/{}.jpg" height="64" width="64"/></td><td><img src="https://webcdn.hirezstudios.com/paladins/champion-headers/{}.png" height="32" width="64"/></td><td><img src="https://webcdn.hirezstudios.com/paladins/assets/carousel/{}.png" height="64" width="32"/></td></tr>'.format(_.get('id'), _.get('feName'), _.get('role').split(' ', 1)[1].replace('er', ''), _.get('title'), _name, _name, _name)
+try:
+  _json_ = [f'{create_value(_)}' for _ in sorted(httpx.get('https://cms.paladins.com/wp-json/api/champion-hub/1').json() or {}, key=lambda x: x.get('id')) if _]
+except:
+  _json_ = [f'{create_value(_)}' for _ in httpx.get('https://cms.paladins.com/wp-json/api/champion-hub/1').json() or {} if _]
 __ = template.format(CHAMPS='\n    '.join(_json_), LEN=str(len(_json_)), DATE=datetime.utcnow().strftime('%m/%d/%Y %H:%M:%S'))
 try:
   #https://stackoverflow.com/a/25476462
